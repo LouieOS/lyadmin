2901c397a77dcf61c386d3c5df60a2485512da64
gashapwn <gashapwn@lingy.in>
2020-11-28 20:12:32 +0000

    added provision.pl. added check for /etc/passwd admin user.

---
+++ perl-script/provision.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+# This script is intended to be run on a fresh
+# OpenBSD install
+
+my $admin_un;
+
+# Check /etc/passwd for the username created during
+# installation
+if( ($admin_un) = `tail /etc/passwd` =~ /^([^:]+):[^:]+:[0-9]{4,}/){
+    printf("admin user will be set to %s\n", $admin_un);
+}else{
+    die "create a non-root user & set user passsword before running this script."
+}
+
