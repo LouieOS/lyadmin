39b5e46b028774d4b1627edbe1851eb79f9b7a4b
gashapwn <gashapwn@lingy.in>
2020-11-28 20:50:18 +0000

    provision.pl: added check for root user. added author header

--- perl-script/provision.pl
+++ perl-script/provision.pl
@@ -3,10 +3,26 @@
 use warnings;
 use strict;
 
+# provision.pl
+# script to provision a tilde instance
+# 
 # This script is intended to be run on a fresh
 # OpenBSD install
+#
+# gashapwn
+# Nov 2020
 
+
+my $pwuid;
 my $admin_un;
+
+
+# Make sure we're running as root
+$pwuid = getpwuid( $< );
+
+if($pwuid ne "root"){
+    die "script must be run as root";
+}
 
 # Check /etc/passwd for the username created during
 # installation
