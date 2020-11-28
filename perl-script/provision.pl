e471a97a13c172cb9726006ac5d663616e692ebb
gashapwn <gashapwn@lingy.in>
2020-11-28 20:28:55 +0000

    fixed regex in provision.pl

--- perl-script/provision.pl
+++ perl-script/provision.pl
@@ -10,7 +10,7 @@ my $admin_un;
 
 # Check /etc/passwd for the username created during
 # installation
-if( ($admin_un) = `tail /etc/passwd` =~ /^([^:]+):[^:]+:[0-9]{4,}/){
+if( ($admin_un) = `tail /etc/passwd` =~ /([^:]+):[^:]+:[0-9]{4,}/){
     printf("admin user will be set to %s\n", $admin_un);
 }else{
     die "create a non-root user & set user passsword before running this script."
