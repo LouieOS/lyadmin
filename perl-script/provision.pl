0cb0b4deb215523dfa70dfd46ecdd587eeb37960
gashapwn <gashapwn@lingy.in>
2020-11-29 07:53:47 +0000

    provision.pl - added logic to setup apache

--- perl-script/provision.pl
+++ perl-script/provision.pl
@@ -58,3 +58,25 @@ system("su $admin_un -c 'python3 -m venv venv'");
 system("su $admin_un -c '. ./venv/bin/activate && pip3 install -r requirements.txt'");
 
 system("pkg_add p5-JSON");
+
+# Install apache
+system("pkg_add apache-httpd");
+
+# enable the userdir module
+system("sed -i -e 's/^\(.\)*#\(LoadModule userdir_module\)/\1\2/' /etc/apache2/httpd2.conf");
+system("sed -i -e 's/^\(.\)*#\(Include \/etc\/apache2\/extra\/httpd-userdir.conf\)/\1\2/' /etc/apache2/httpd2.conf");
+# Enable the CGI directory
+system("echo '<Directory \"/home/*/public_html/cgi/\">
+    Require all granted
+    Options +ExecCGI
+    AddHandler cgi-script .cgi
+</Directory>' >> /etc/apache2/extra/httpd-userdir.conf");
+# Enable the CGI modules
+system("sed -i -e 's/^\(.\)*#\(LoadModule cgi_module\)/\1\2/' /etc/apache2/httpd2.conf");
+system("sed -i -e 's/^\(.\)*#\(LoadModule cgid_module\)/\1\2/' /etc/apache2/httpd2.conf");
+# Disable directory listing
+system("sed -i -e 's/\(<\/Directory>\)/    Options -Indexes\
+       \1/g' /etc/apache2/extra/httpd-userdir.conf");
+
+# Change the port to 5001
+system("sed -i -e 's/^\(.\)*Listen *80/\1Listen 5001/' /etc/apache2/httpd2.conf");
